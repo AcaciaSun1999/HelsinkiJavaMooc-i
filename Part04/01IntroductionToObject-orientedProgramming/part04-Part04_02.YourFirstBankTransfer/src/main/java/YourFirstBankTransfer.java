fa7de73adc2514ada7f677fda9c51dd2d13dac8b
AcaciaSun1999 <85231957+AcaciaSun1999@users.noreply.github.com>
2023-06-01 23:09:25 +0800

    Add files via upload

---
+++ Part04/01IntroductionToObject-orientedProgramming/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -0,0 +1,14 @@
+
+public class YourFirstBankTransfer {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // write your program here
+        Account MatthewsAccount = new Account("Matthews account", 1000.00);
+        Account MyAccount = new Account("My account", 0.00);
+        MatthewsAccount.withdrawal(100);
+        MyAccount.deposit(100);
+        System.out.println(MatthewsAccount);
+        System.out.println(MyAccount);
+    }
+}
