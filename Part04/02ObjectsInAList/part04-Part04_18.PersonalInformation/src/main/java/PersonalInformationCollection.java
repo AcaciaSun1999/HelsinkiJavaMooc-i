8c95990b504f9c780f5726fae5830466ce8d5227
AcaciaSun1999 <85231957+AcaciaSun1999@users.noreply.github.com>
2023-06-03 22:00:45 +0800

    Add files via upload

---
+++ Part04/02ObjectsInAList/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PersonalInformationCollection {
+
+    public static void main(String[] args) {
+        // implement here your program that uses the PersonalInformation class
+
+        ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("Firstname: ");
+            String firstName = scanner.nextLine();
+            if(firstName.equals("")){
+                break;
+            }
+            System.out.println("Lastname: ");
+            String lastName = scanner.nextLine();
+            System.out.println("Identification number: ");
+            String ID = scanner.nextLine();
+            infoCollection.add(new PersonalInformation(firstName, lastName, ID));
+        }
+        
+        for(int i = 0; i < infoCollection.size(); i++){
+            System.out.println(infoCollection.get(i).getFirstName() + " " + infoCollection.get(i).getLastName());
+        }
+    }
+}
